411719ef9b8a661bebbda01734ef271d211007ad
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-15 18:28:54 -0800

    Create Clickwrap.js

---
+++ Clickwrap.js
@@ -0,0 +1,15 @@
+    <div id="ds-clickwrap"></div>
+
+    <script src="https://demo.docusign.net/clickapi/sdk/latest/docusign-click.js"></script>
+
+    <script>docuSignClick.Clickwrap.render
+  (
+    {
+      environment: 'https://demo.docusign.net',
+      accountId: '9939cecb-8503-4215-a19e-38996d165de3',
+      clickwrapId: '14db86e1-4c03-427e-b911-15f2ab0fbb27',
+      clientUserId: 'UNIQUE_USER_ID'
+    }
+      ,'#ds-clickwrap');
+    </script>
+  
